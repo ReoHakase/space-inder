8a3c23f11ea84660614e25e3d4ecb55ae0237926
H-Tsukasa <mip68556@gmail.com>
2023-09-01 16:39:13 +0900

    fix: 🐛 エラー修正

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,10 +2,10 @@ import type { FC } from 'react';
 
 const Home: FC = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-4 text-keyplate-12">
+    <div className="text-keyplate-12 flex min-h-screen flex-col items-center justify-center gap-4">
       <article className="flex max-w-2xl flex-col gap-4 p-6">
         <section>
-          <p className="w-full shrink-0 text-2xl font-bold text-center">
+          <p className="w-full shrink-0 text-center text-2xl font-bold">
             マッチング率を判断したい<br></br> キーワードを入力してください。
           </p>
         </section>
