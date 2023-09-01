88dd27b90dfbb95440256efd5294a4c422686115
H-Tsukasa <mip68556@gmail.com>
2023-09-01 16:45:29 +0900

    fix: 🐛 修正

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,7 +2,7 @@ import type { FC } from 'react';
 
 const Home: FC = () => {
   return (
-    <div className="text-keyplate-12 flex min-h-screen flex-col items-center justify-center gap-4">
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4 text-keyplate-12">
       <article className="flex max-w-2xl flex-col gap-4 p-6">
         <section>
           <p className="w-full shrink-0 text-center text-2xl font-bold">
